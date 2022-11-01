b9850686f3aa44c4fb4b1f10889cb77934c35a3c
semindn <semindn@yandex.ru>
2022-11-01 10:17:00 +0300

    Решение 2.1.3

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,12 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
-    }///
+
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat1 = applicationContext.getBean("cat", Cat.class);
+        Cat cat2 = applicationContext.getBean("cat", Cat.class);
+        System.out.println(bean == bean1);
+        System.out.println(cat1 == cat2);
+    }
 }
